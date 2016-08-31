96610c2984a34920f1f005968420f982d0556c91
Joe Phearse <jphearse@gmail.com>
2016-08-31 12:14:05 -0500

    checks for scalar

--- foreach.php
+++ foreach.php
@@ -3,14 +3,8 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing){
-    if (is_numeric($thing)) {
-        echo "{$thing} is a number\n";
-    } else if (is_string($thing)) {
-        echo "{$thing} is a string\n";
-    } else if (is_float($thing)) {
-        echo "{$thing} is float\n";
-    }else if (is_array($thing)) {
-        echo "{$thing} is an array\n";
+    if (is_scalar($thing)) {
+        echo "{$thing} is a scalar\n";
     }
 
 }
