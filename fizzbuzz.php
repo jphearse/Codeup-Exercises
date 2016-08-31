c9ae5830b3a36b610b56490492dd7f64a8b5aff9
Joe Phearse <jphearse@gmail.com>
2016-08-31 11:39:09 -0500

    added comments

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -3,13 +3,13 @@ $startingNumber = 1;
 $endingNumber = 100;
 
 for($startingNumber; $startingNumber <= $endingNumber; $startingNumber++){
-	if($startingNumber % 3 == 0 && $startingNumber % 5 == 0){
+	if($startingNumber % 3 == 0 && $startingNumber % 5 == 0){			//if multiple of 3 or 5 FizzBuzz
 		echo "FizzBuzz\n";
-	} elseif ($startingNumber % 3 == 0) {
+	} elseif ($startingNumber % 3 == 0) {			// if multiple of 3 Fizz
 		echo "Fizz\n";
-	} elseif($startingNumber % 5 == 0){
+	} elseif($startingNumber % 5 == 0){				// if multiple of 5 Buzz
 		echo "Buzz\n";
 	} else {
-		echo "$startingNumber\n";
+		echo "$startingNumber\n";					// if neither print number
 	}
 }
