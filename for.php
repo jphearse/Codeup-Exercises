78d991f84fadafb3ea8de246be9218395d86c685
Joe Phearse <jphearse@gmail.com>
2016-08-31 11:27:28 -0500

    catch if second number is the same as first

--- for.php
+++ for.php
@@ -1,33 +1,34 @@
 <?php 
 do{
-	fwrite(STDOUT, "Starting Number: ");
+	fwrite(STDOUT, "Starting Number: ");		//ask for first number, keep asking until get number
 	$firstNumber = trim(fgets(STDIN));
 } while(!is_numeric($firstNumber));
 
 do{
-	fwrite(STDOUT, "Ending Number: ");
+	fwrite(STDOUT, "Ending Number: ");			//ask for second number, keep asking until get number
 	$secondNumber = trim(fgets(STDIN));
-} while(!is_numeric($secondNumber));
+	if($secondNumber == $firstNumber){			//if second number is same get new one
+		echo "Same number as first. Enter new number.\n";
+	}
+} while(!is_numeric($secondNumber) || $secondNumber == $firstNumber);
 
-if($firstNumber < $secondNumber){
+if($firstNumber < $secondNumber){				//if first number is less than second increment
 	fwrite(STDOUT, "Increment by: ");
 	$increment = trim(fgets(STDIN));
 
-	(is_numeric($increment)) ? $increment = $increment : $increment = 1;
+	(is_numeric($increment)) ? $increment = $increment : $increment = 1;	//check if number
 
-	for($firstNumber; $firstNumber <= $secondNumber; $firstNumber += $increment){
+	for($firstNumber; $firstNumber <= $secondNumber; $firstNumber += $increment){	//outputs numbers
 		echo "$firstNumber\n";
 	}
-} elseif ($firstNumber > $secondNumber){
+} else {		//if first numer is bigger than second decrement
 	fwrite(STDOUT, "Decrement by: ");
 	$decrement = trim(fgets(STDIN));
 
-	(is_numeric($decrement)) ? $decrement = $decrement : $decrement = 1;
+	(is_numeric($decrement)) ? $decrement = $decrement : $decrement = 1; //check if number
 
-	for($firstNumber; $firstNumber >= $secondNumber; $firstNumber -= $decrement){
+	for($firstNumber; $firstNumber >= $secondNumber; $firstNumber -= $decrement){	//outputs numbers
 		echo "$firstNumber\n";
 	}	
-} else {
-	echo "Same numbers entered\n";
 }
 
