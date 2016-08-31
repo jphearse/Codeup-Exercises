f9e357dbd1f8d49adfd853acf465fae38d6a64c9
Joe Phearse <jphearse@gmail.com>
2016-08-31 14:02:52 -0500

    cleaned code, simplified

--- foreach.php
+++ foreach.php
@@ -3,8 +3,16 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing){
-    if (is_scalar($thing)) {
-        echo "{$thing} is a scalar\n";
+    if (is_array($thing)) {
+        echo gettype($things) . " (";
+        	foreach ($thing as $number){
+        		echo  $number;
+        	}
+        echo ")\n";
+    } elseif (is_scalar($thing)) {
+       echo "$thing\n";
+    } elseif (is_null($thing)) {
+       echo "$thing\n";
     }
 
 }
