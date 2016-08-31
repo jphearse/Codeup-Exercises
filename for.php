62e60de711633f3e7d38e0c6d9487145060a90b1
Joe Phearse <jphearse@gmail.com>
2016-08-31 10:57:34 -0500

    added decrement feature

--- for.php
+++ for.php
@@ -9,11 +9,24 @@ fwrite(STDOUT, "Ending Number: ");
 $secondNumber = trim(fgets(STDIN));
 } while(!is_numeric($secondNumber));
 
-fwrite(STDOUT, "Increment by: ");
-$increment = trim(fgets(STDIN));
+if($firstNumber < $secondNumber){
+	fwrite(STDOUT, "Increment by: ");
+	$increment = trim(fgets(STDIN));
 
-(is_numeric($increment)) ? $increment = $increment : $increment = 1;
+	(is_numeric($increment)) ? $increment = $increment : $increment = 1;
 
-for($firstNumber; $firstNumber <= $secondNumber; $firstNumber+= $increment){
-	echo "$firstNumber\n";
+	for($firstNumber; $firstNumber <= $secondNumber; $firstNumber += $increment){
+		echo "$firstNumber\n";
+	}
 }
+else {
+	fwrite(STDOUT, "Decrement by: ");
+	$decrement = trim(fgets(STDIN));
+
+	(is_numeric($decrement)) ? $decrement = $decrement : $decrement = 1;
+
+	for($firstNumber; $firstNumber >= $secondNumber; $firstNumber -= $decrement){
+		echo "$firstNumber\n";
+	}	
+}
+
