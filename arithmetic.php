aacbefa9aa2e3f28b11a1cc9668e2deb092602b2
Joe Phearse <jphearse@gmail.com>
2016-09-01 14:12:24 -0500

    added error functions, reads inputs and puts out correct error message

--- arithmetic.php
+++ arithmetic.php
@@ -2,46 +2,86 @@
 
 function add($a, $b)
 {
-    return $a + $b;
+	if( is_numeric($a) && is_numeric($b)){
+    	return $a + $b;
+    } else{
+    	return errorNotNumber();
+    }
 }
 
 function subtract($a, $b)
 {
-    return $a - $b;
+    if( is_numeric($a) && is_numeric($b)){
+    	return $a - $b;
+    } else{
+    	return errorNotNumber();
+    }
 }
 
 function multiply($a, $b)
 {
-	return $a * $b;
+	if( is_numeric($a) && is_numeric($b)){
+    	return $a * $b;
+    } else{
+    	return errorNotNumber();
+    }
 }
 
 function divide($a, $b)
 {
-	return $a / $b;
+	if($b === 0){
+    	return errorByZero();
+    } else if(is_numeric($a) && is_numeric($b)){
+    	return $a / $b;
+    }else{
+    	return errorNotNumber();
+    }
 }
 
 function modulus($a, $b)
 {
-	return $a / $b;
+	if($b === 0){
+    	return errorByZero();
+    } else if(is_numeric($a) && is_numeric($b)){
+    	return $a % $b;
+    }else{
+    	return errorNotNumber();
+    }
 }
 
 function square($a)
 {
-	return $a * $a;
+	if( is_numeric($a)){
+    	return $a * $a;
+    } else{
+    	return errorNotNumber();
+    }
 }
 
 function sqroot($a)
 {
-	return sqrt($a);
+	if( is_numeric($a)){
+    	return sqrt($a);
+    } else{
+    	return errorNotNumber();
+    }
+}
+
+function errorByZero (){
+	return "Error: Dividing by 0";	
+}
+function errorNotNumber(){
+	return "Error: Inputs not numbers";
 }
 
 $a = 20;
-$b = 10;
+$b = "hi";
 
-echo add($a, $b) .PHP_EOL;
-echo subtract($a, $b) .PHP_EOL;
-echo multiply($a, $b) .PHP_EOL;
-echo divide($a, $b) .PHP_EOL;
-echo modulus($a, $b) .PHP_EOL;
-echo square($a) .PHP_EOL;
-echo sqroot($a) .PHP_EOL;
+echo "First Number: $a Second Number: $b" . PHP_EOL;
+echo "Add: ". add($a, $b) .PHP_EOL;
+echo "Subtract: ".  subtract($a, $b) .PHP_EOL;
+echo "Multiply: ". multiply($a, $b) .PHP_EOL;
+echo "Divide: ". divide($a, $b) .PHP_EOL;
+echo "Modulus: ". modulus($a, $b) .PHP_EOL;
+echo "Square: ". square($a) .PHP_EOL;
+echo "Square Root: ". sqroot($a) .PHP_EOL;
