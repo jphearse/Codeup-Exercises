250fc4cca2edf26ba6658c3bb78eb3c3f382a2c9
Joe Phearse <jphearse@gmail.com>
2016-09-02 16:30:03 -0500

    cleaned code

--- list_with_commas.php
+++ list_with_commas.php
@@ -6,14 +6,14 @@ function humanizedList($string, $sort = false){
 	$physicistsArray = explode (", " , $string);	//puts string into array
 
 	if ($sort == "sort"){
-		sort($physicistsArray);							//sorts array by names
+		sort($physicistsArray);						//sorts array by names
 	}
 
 	$lastName = array_pop($physicistsArray);		//takes last name off
-	array_push($physicistsArray, "and ".$lastName);//pushes "and " and the last name on
+	array_push($physicistsArray, "and ".$lastName);	//pushes "and " and the last name on
 
-	$famousFakePhysicists = implode(", ", $physicistsArray); //breaks array with comma and space
-
-	echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.\n";
+	return implode(", ", $physicistsArray); 		//breaks array with comma and space
 }
-humanizedList($physicistsString);
+
+$famousFakePhysicists = humanizedList($physicistsString, "sort");
+echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}." . PHP_EOL;
