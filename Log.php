93811156c6e9343865712dac483d471d93d42647
Joe Phearse <jphearse@gmail.com>
2016-09-13 13:49:08 -0500

    initial commit

---
+++ Log.php
@@ -0,0 +1,29 @@
+<?php 
+
+class Log 
+{
+	public $filename;
+
+	public function logMessage($logLevel, $message)
+	{
+	    // todo - complete this function
+
+		$handle = fopen($this->filename, 'a');
+		
+		date_default_timezone_set('America/Chicago');
+		// $date = new DateTime();
+		$date = date("Y/m/d h:i:s");
+
+		fwrite($handle, $date . " " .$logLevel .": ".$message. PHP_EOL . "-----------------------------------------------------" . PHP_EOL);
+		fclose($handle);
+	}
+
+	public function info($message){
+		$this->logMessage("INFO", $message);
+	}
+	public function error($message){
+		$this->logMessage("ERROR", $message);
+	}
+}
+
+?>
---
+++ log_test.php
@@ -0,0 +1,8 @@
+<?php 
+require_once 'Log.php';
+
+$instance = new Log();
+
+$instance->filename = "log-". date('Y-m-d') . '.log';
+$instance->info("User logged in successfully");
+$instance->error("User or password is incorrect");
