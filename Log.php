cf0bcbc4e6e9d26ef550afc08e4ed3ae54b66126
Joe Phearse <jphearse@gmail.com>
2016-09-13 15:41:45 -0500

    added construct and destruct

--- Log.php
+++ Log.php
@@ -3,18 +3,24 @@
 class Log 
 {
 	public $filename;
+	public $handle;
+	function __construct ($prefix = 'log')
+	{	
+		$this->filename = $prefix ."-". date('Y-m-d') . '.log';
+		$this->handle = fopen($this->filename, 'a');
+	}
+
+	function __destruct(){
+		fclose($this->handle);
+	}
 
 	public function logMessage($logLevel, $message)
-	{
-	    // todo - complete this function
-		$handle = fopen($this->filename, 'a');
-		
+	{		
 		date_default_timezone_set('America/Chicago');
-		// $date = new DateTime();
+
 		$date = date("Y/m/d h:i:s");
 
-		fwrite($handle, $date . " " .$logLevel .": ".$message. PHP_EOL . "-----------------------------------------------------" . PHP_EOL);
-		fclose($handle);
+		fwrite($this->handle, $date . " " .$logLevel .": ".$message. PHP_EOL . "-----------------------------------------------------" . PHP_EOL);
 	}
 
 	public function info($message){
