5039b5fbe93f598801ad538a3261557fdf6ecfd9
Joe Phearse <jphearse@gmail.com>
2016-09-28 14:06:34 -0500

    commented out line

--- log_test.php
+++ log_test.php
@@ -3,6 +3,6 @@ require_once 'Log.php';
 
 $instance = new Log('cli');
 
-$instance->filename = "log-". date('Y-m-d') . '.log';
+// $instance->filename = "log-". date('Y-m-d') . '.log';
 $instance->info("User logged in successfully");
 $instance->error("User or password is incorrect");
